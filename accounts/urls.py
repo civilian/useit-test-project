2db2f700fbcd7519dede79a0003e268685961171
oscarchamatc <chamatoscar@gmail.com>
2019-03-09 20:47:22 -0500

    logout working

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ from accounts import views
 app_name = 'accounts'
 urlpatterns = [
     path('signup', views.SignUp.as_view(), name='signup'),
-    path('login', auth_views.LoginView.as_view(redirect_field_name='/'), name='login'),
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
